4b51a95c27982df3515edec48eae4af6c97560d6
Adrian-Paul Carrières <adrian.paul.carrieres@gmail.com>
2021-12-10 18:50:18 +0100

    better ui

--- front/components/Base.js
+++ front/components/Base.js
@@ -45,28 +45,40 @@ const GithubProfile = ({ route, navigation }) => {
     const { user } = route.params
 
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>{user.name}</Text>
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image
+                    source={{
+                        uri: user.avatar_url,
+                    }}
+                    style={{ width: 100, height: 100 }}
+                />
+                <View style={{ flewDirection: "column", margin: "5%", justifyContent: "center" }}>
+                    <Text>{user.name}</Text>
+                    <Text style={{ fontSize: 10 }}>{user.login}</Text>
+                </View>
+            </View>
 
-            <Text>{user.login}</Text>
-            <Text>🏢 {user.company}</Text>
-            <Text>📭 {user.location}</Text>
-            <Text>🧑 {user.bio}</Text>
-            <Text><SocialIcon
-                raised={false}
-                iconSize={5}
-                type='twitter'
-            />{user.twitter_username}</Text>
-            <Text>📚 {user.public_repos} public repos</Text>
+            <View style={styles.content}>
 
-            <Text>🧑‍🤝‍🧑 {user.followers} followers · {user.following} following</Text>
-            <Text>	📆 joined the {user.created_at.split("T")[0]}</Text>
+                <Text>🏢 {user.company}</Text>
+                <Text>📭 {user.location}</Text>
+                <Text>🧑 {user.bio}</Text>
+                <Text><SocialIcon
+                    raised={false}
+                    iconSize={5}
+                    type='twitter'
+                />{user.twitter_username}</Text>
+                <Text>📚 {user.public_repos} public repos</Text>
 
-            <Image
-                source={{
-                    uri: user.avatar_url,
-                }}
-                style={{ width: 100, height: 100 }}
+                <Text>🧑‍🤝‍🧑 {user.followers} followers · {user.following} following</Text>
+                <Text>	📆 joined the {user.created_at.split("T")[0]}</Text>
+
+            </View>
+            <SocialIcon
+                title='Go to my profile'
+                button
+                type='github'
             />
         </View>
     );
@@ -82,6 +94,20 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         padding: 10,
     },
+    container: {
+        flex: 1,
+        padding: "5%"
+    },
+    header: {
+        flexDirection: "row",
+    },
+    content: {
+        flex: 1,
+        padding: "5%",
+        justifyContent: "center",
+        alignContent: "center",
+        alignItems: "center"
+    }
 });
 
 const Stack = createNativeStackNavigator();
